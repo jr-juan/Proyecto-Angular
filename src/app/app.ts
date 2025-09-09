54cea15e72217078319ee809fa908906c1d405e4
hale-ale7 <halexandraestupinan@unipacifico.edu.co>
2025-09-09 16:02:02 -0500

    Modificado para la entrega

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,28 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AgregarTareaComponent } from './semana5/componentes/agregar_tarea/agregar_tarea.component';
+import { ListaTareaComponent } from './semana5/componentes/lista_tarea/lista_tarea.component';
+import { Tarea } from './semana5/estados/tarea_model';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [AgregarTareaComponent, ListaTareaComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
-  protected readonly title = signal('PrimerProyecto');
+  
+  tareas = signal<Tarea[]>([]);
+
+  agregarTarea(tarea: Tarea) {
+    this.tareas.update((lista) => [...lista, tarea]);
+  }
+
+  eliminarTarea(id: number) {
+    this.tareas.update((lista) => lista.filter(t => t.id !== id));
+  }
+
+  total() {
+    return this.tareas().length;
+  }
 }
