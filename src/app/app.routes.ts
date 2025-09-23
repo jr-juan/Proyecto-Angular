d38b18ec8672abaafd13aee1d3eb0a4cd4cb3ac6
Juan Cuero <juanromancuero@gmail.com>
2025-09-23 01:01:57 -0500

    Adaptado para funcionamiento correcto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
+// app.routes.ts
 import { Routes } from '@angular/router';
+import { InicioComponent} from './semana7/paginas/inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: InicioComponent},
+  {path: 'semana7', component: InicioComponent}, 
+  {path: '**', redirectTo: ''} // Sino encuentra alguna ruta, va al inicio.
+];
