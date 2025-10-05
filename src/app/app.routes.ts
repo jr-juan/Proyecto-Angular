9fec4a65a8cbc3694a697d9c79e84685c96e9e32
Juan Cuero <juanromancuero@gmail.com>
2025-10-05 18:57:18 -0500

    modificado para la implementacion de la api con nuevas funciones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
-// app.routes.ts
 import { Routes } from '@angular/router';
-import { InicioComponent} from './semana7/paginas/inicio.component';
+import { TableroComponent } from './semana7/paginas/tablero.component';
+import { VehiculosComponent } from './semana7/paginas/vehiculos.component';
+import { RutasComponent } from './semana7/paginas/rutas.component';
+import { LoginComponent } from './semana7/inicio_y_registro/login.component';
+import { RegistroComponent } from './semana7/inicio_y_registro/registro.component';
 
 export const routes: Routes = [
-  {path: '', component: InicioComponent},
-  {path: 'semana7', component: InicioComponent}, 
-  {path: '**', redirectTo: ''} // Sino encuentra alguna ruta, va al inicio.
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'tablero', component: TableroComponent },
+  { path: 'vehiculos', component: VehiculosComponent },
+  { path: 'rutas', component: RutasComponent },
+  { path: '**', redirectTo: 'login' }
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet, Router, NavigationEnd} from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('PrimerProyecto');
+
+  mostrarHeader = signal(true);
+  // Detectar cambios de ruta para mostrar/ocultar el header del app.html
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        const rutasPublicas = ['/login', '/registro'];
+        this.mostrarHeader.set(!rutasPublicas.includes(event.url));
+      });
+  }
 }
