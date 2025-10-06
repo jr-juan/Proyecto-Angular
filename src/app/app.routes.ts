3753faf5fc2609ebeebae4bbce911568a872ce72
Juan Cuero <juanromancuero@gmail.com>
2025-10-05 20:13:44 -0500

    modificado para la implementacion de funciones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./semana9/optimizacion.routes')
+        .then(m => m.RUTAS_OPTIMIZACION)
+  },
+  { path: '**', redirectTo: '' }
+];
