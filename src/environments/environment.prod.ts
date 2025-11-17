61dcd2aa4aabdafa861da96740cd63af133124d5
Juan Cuero <juanromancuero@gmail.com>
2025-11-17 03:22:06 -0500

    Agregando archivo de variables de entorno para proteger el id de la API, URL de la API y el Token de Map Box

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environmentApi = {
+    produccion: true,
+    apiUrl: 'http://apirecoleccion.gonzaloandreslucio.com/api'
+};
+
+export const environmentPerfilId = {
+    produccion: true,
+    perfilId: '321d109f-6396-470a-b30a-ed347f8842c9'
+};
+
+export const environment = {
+  production: true,
+  mapboxToken: 'pk.eyJ1IjoianJjdWVybyIsImEiOiJjbWkybnV6YjIxY2VjMmpweWczM24wMm5sIn0.LPQ5Q6_iU6jpGbjmgd5NgA'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environmentApi = {
+    produccion: false,
+    apiUrl: 'http://apirecoleccion.gonzaloandreslucio.com/api'
+};
+
+export const environmentPerfilId = {
+    produccion: false,
+    perfilId: '321d109f-6396-470a-b30a-ed347f8842c9'
+};
+
+export const environment = {
+  production: false,
+  mapboxToken: 'pk.eyJ1IjoianJjdWVybyIsImEiOiJjbWkybnV6YjIxY2VjMmpweWczM24wMm5sIn0.LPQ5Q6_iU6jpGbjmgd5NgA'
+};
