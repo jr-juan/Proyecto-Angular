7bedafbd281b64c61d703ba0d57960bc843bfb8d
Juan Cuero <juanromancuero@gmail.com>
2026-02-28 20:57:56 -0500

    Agregando logica para cerrar sesion con el modal

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,43 +3,53 @@ import { RouterModule, RouterOutlet, Router, NavigationEnd } from '@angular/rout
 import { filter } from 'rxjs/operators';
 import { AuthService } from './semana7/servicios/auth';
 
-
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App implements OnInit {
   protected readonly title = signal('PrimerProyecto');
 
   mostrarHeader = signal(true);
+
+  mostrarModal = signal(false);
+
+  
   rolUsuario = signal<'admin' | 'chofer' | null>(null);
   // Detectar cambios de ruta para mostrar/ocultar el header del app.html
-  constructor(private router: Router, public authService: AuthService) {
+  constructor(
+    private router: Router,
+    public authService: AuthService,
+  ) {
     this.router.events
-      .pipe(filter(event => event instanceof NavigationEnd))
+      .pipe(filter((event) => event instanceof NavigationEnd))
       .subscribe((event: any) => {
         const rutasPublicas = ['/login', '/registro'];
         this.mostrarHeader.set(!rutasPublicas.includes(event.url));
       });
   }
 
-
   // Suscribirse al rol del usuario
   ngOnInit() {
-    this.authService.rolUsuario$.subscribe(rol => {
+    this.authService.rolUsuario$.subscribe((rol) => {
       this.rolUsuario.set(rol);
     });
   }
 
-  async cerrarSesion() {
-    // Pedimos confirmación al usuario con un diálogo simple del navegador
-    const confirmacion = confirm('¿Estás seguro de que quieres cerrar sesión?');
+  
 
-    if (confirmacion) {
-      // Si el usuario hace clic en "Aceptar", llamamos a la función logout del servicio
-      await this.authService.logout();
-    }
+  cerrarSesion() {
+    this.mostrarModal.set(true);
+  }
+
+  cancelarCerrarSesion() {
+    this.mostrarModal.set(false);
+  }
+
+  async confirmarCerrarSesion() {
+    this.mostrarModal.set(false);
+    await this.authService.logout();
   }
 }
