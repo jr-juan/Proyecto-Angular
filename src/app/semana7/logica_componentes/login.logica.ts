5f763803b1d59e97332b34c0c165ea59d3625ed1
Juan Cuero <juanromancuero@gmail.com>
2026-02-28 19:37:39 -0500

    Refactorizacion de la logica en el login

--- src/app/semana7/logica_componentes/login.logica.ts
+++ src/app/semana7/logica_componentes/login.logica.ts
@@ -18,31 +18,27 @@ export class LoginComponent {
   constructor(private authService: AuthService, private router: Router) {}
 
   async iniciarSesion() {
-    if (!this.email || !this.password) {
-      alert('Por favor, ingresa correo y contraseña.');
-      return;
-    }
-
-    const user = await this.authService.login({
-      email: this.email,
-      password: this.password,
-    });
-
-    if (user) {
-      // Esperar un pequeño delay para asegurar que el rol se cargó
-      await new Promise(resolve => setTimeout(resolve, 300));
-      
-      const rol = this.authService.rolActual;
-      
-      console.log('Rol que inicio sesion:', rol); 
-
-      if (rol === 'admin') {
-        this.router.navigate(['/tablero']);
-      } else {
-        this.router.navigate(['/chofer']);
-      }
-    } else {
-      alert('Error en el correo o la contraseña. Por favor, inténtalo de nuevo.');
-    }
+  if (!this.email || !this.password) {
+    alert('Por favor, ingresa correo y contraseña.');
+    return;
   }
+
+  const user = await this.authService.login({
+    email: this.email,
+    password: this.password,
+  });
+
+  if (user) {
+    const rol = this.authService.rolActual; // ya viene del await login()
+    console.log('Rol que inicio sesion:', rol);
+
+    if (rol === 'admin') {
+      this.router.navigate(['/tablero']);
+    } else {
+      this.router.navigate(['/chofer']);
+    }
+  } else {
+    alert('Error en el correo o la contraseña. Por favor, inténtalo de nuevo.');
+  }
+}
 }
